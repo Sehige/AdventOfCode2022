422f3eaa03ffc159238d3803466b118774e3e25a
sbonta <sergiua98@gamil.com>
2022-12-22 01:00:21 +0200

    Day 11 part 1 solved, part 2 too much stress

--- AdventOfCode2022/Day11/Day11.cs
+++ AdventOfCode2022/Day11/Day11.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using static AdventOfCode.Day11;
 
 namespace AdventOfCode
 {
@@ -9,19 +12,21 @@ namespace AdventOfCode
     {
         public struct Monkey
         {
-            public List<float> listItems;
+            public List<BigInteger> listItems;
             public string[] sOperation;
-            public int nDivider;
+            public BigInteger nDivider;
             public int nTrueMonkey;
             public int nFalseMonkey;
+            public int nInspections;
 
             public Monkey()
             {
-                listItems = new List<float>();
+                listItems = new List<BigInteger>();
                 sOperation= new string[2];
                 nDivider = 1;
                 nTrueMonkey = 0;
                 nFalseMonkey = 0;
+                nInspections = 0;
             }
         }
 
@@ -30,11 +35,88 @@ namespace AdventOfCode
             string workingDirectory = Environment.CurrentDirectory;
             string sDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
             string[] sText = File.ReadAllLines(sDirectory + "\\day11\\Day11.txt");
+            BigInteger dStress = 0;
+            int nMonkey = 0;
+            float fInspections = 0;
+            List<float> listInspections = new List<float>();
+            //Dictionary<int, Monkey> keyMonkey = new Dictionary<int, Monkey>();
+            List<Monkey> listMonkey = GetMonkey(sText);
 
-            Dictionary<int, Monkey> keyMonkey = new Dictionary<int, Monkey>();
+            foreach (Monkey monkey in listMonkey)
+            {
+                listInspections.Add(0);
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                nMonkey = 0;
+                foreach (Monkey monkey in listMonkey)
+                {
+                    if (monkey.listItems.Count == 0)
+                    {
+                        nMonkey++;
+                        continue;
+                    }
+
+                    fInspections = monkey.listItems.Count;
+
+                    foreach (BigInteger dItem in monkey.listItems)
+                    {
+                        if (monkey.sOperation[1] == "*")
+                        {
+                            if (monkey.sOperation[0] == monkey.sOperation[1])
+                            {
+                                dStress = dItem * dItem;
+                            }
+                            else
+                            {
+                                dStress = dItem * BigInteger.Parse(monkey.sOperation[0]);
+                            }
+                        }
+                        else
+                        {
+                            if (monkey.sOperation[0] == monkey.sOperation[1])
+                            {
+                                dStress = dItem + dItem;
+                            }
+                            else
+                            {
+                                dStress = dItem + BigInteger.Parse(monkey.sOperation[0]);
+                            }
+                        }
+
+                        //dStress = Math.Floor(dStress/ 3);
+
+                        if (dStress % monkey.nDivider == 0)
+                        {
+                            listMonkey[monkey.nTrueMonkey].listItems.Add(dStress);
+                            //Console.WriteLine("M {2}| Stress level :{0}, to Monkey{1}", dStress, monkey.nTrueMonkey, nMonkey);
+                        }
+                        else
+                        {
+                            listMonkey[monkey.nFalseMonkey].listItems.Add(dStress);
+                            //Console.WriteLine("M {2}| Stress level :{0}, to Monkey{1}", dStress, monkey.nFalseMonkey, nMonkey);
+                        }
+                    }
+
+                    listInspections[nMonkey] += fInspections;
+                    if (listInspections[2] > 8)
+                    {
+
+                    }
+                    monkey.listItems.Clear();
+                    nMonkey++;
+                }
+            }
+
+            WriteProdMax2(listInspections);
+        }
+
+        private List<Monkey> GetMonkey(string[] sText)
+        {
             List<Monkey> listMonkey = new List<Monkey>();
+
             Monkey buildMonkey = new Monkey();
-            int nIndexMonkey = 0;
 
             foreach (string s in sText)
             {
@@ -42,16 +124,15 @@ namespace AdventOfCode
                 {
                     //listMonkey.Add(buildMonkey);
                     string sNumber = s.Split(' ')[1].Trim(':');
-                    nIndexMonkey = int.Parse(sNumber);
 
                     buildMonkey = new Monkey();
                 }
                 else if (s.Contains("Starting"))
                 {
                     string[] sStarting = s.Split(' ');
-                    foreach(string sNumbers in sStarting)
+                    foreach (string sNumbers in sStarting)
                     {
-                        if (float.TryParse(sNumbers.Trim(','), out float item))
+                        if (BigInteger.TryParse(sNumbers.Trim(','), out BigInteger item))
                         {
                             buildMonkey.listItems.Add(item);
                         }
@@ -62,17 +143,19 @@ namespace AdventOfCode
                     if (s.Contains("old + old"))
                     {
                         buildMonkey.sOperation[0] = "+";
+                        buildMonkey.sOperation[1] = "+";
                     }
                     else if (s.Contains("old * old"))
                     {
                         buildMonkey.sOperation[0] = "*";
+                        buildMonkey.sOperation[1] = "*";
                     }
                     else
                     {
                         string[] sOperation = s.Split(' ');
                         for (int i = 0; i < sOperation.Length; i++)
                         {
-                            if (int.TryParse(sOperation[i], out int nNumber)) 
+                            if (int.TryParse(sOperation[i], out int nNumber))
                             {
                                 buildMonkey.sOperation[0] = nNumber.ToString();
                                 buildMonkey.sOperation[1] = sOperation[i - 1];
@@ -83,9 +166,9 @@ namespace AdventOfCode
                 else if (s.Contains("Test"))
                 {
                     string[] sDiv = s.Split(" ");
-                    foreach(string str in sDiv)
+                    foreach (string str in sDiv)
                     {
-                        if (int.TryParse(str, out int nDiv))
+                        if (BigInteger.TryParse(str, out BigInteger nDiv))
                         {
                             buildMonkey.nDivider = nDiv;
                         }
@@ -115,6 +198,40 @@ namespace AdventOfCode
 
                     listMonkey.Add(buildMonkey);
                 }
+            }
+
+            return listMonkey;
+        }
+
+        private void WriteProdMax2(List<float> listInspections)
+        {
+            float fMax1 = 0;
+            float fMax2 = 0;
+            foreach (float n in listInspections)
+            {
+                if (fMax1 < n)
+                {
+                    fMax2 = fMax1;
+                    fMax1 = n;
+                }
+                else if (fMax2 < n)
+                {
+                    fMax2 = n;
+                }
+            }
+            Console.WriteLine(fMax1+ " " + fMax2);
+            Console.WriteLine(fMax1 * fMax2);
+        }
+
+        private void WriteFinalStress(List<Monkey> listMonkey)
+        {
+            foreach (Monkey monkey in listMonkey)
+            {
+                foreach (float fStress in monkey.listItems)
+                {
+                    Console.Write(fStress + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
